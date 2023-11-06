eb37f9155dcd3b42f3eb4b6f434fe917df14a087
dogukan dinler <dogukan12@gmail.com>
2023-11-07 02:02:46 +0300

    Dto controller mapper repo and service added

--- src/main/java/com/example/employeechecker/Dto/EmployeeDto.java
+++ src/main/java/com/example/employeechecker/Dto/EmployeeDto.java
@@ -4,7 +4,6 @@ package com.example.employeechecker.Dto;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.time.LocalDateTime;
 
 @Getter
 @Setter
--- src/main/java/com/example/employeechecker/Repository/EmployeeRepository.java
+++ src/main/java/com/example/employeechecker/Repository/EmployeeRepository.java
@@ -1,6 +1,7 @@
 package com.example.employeechecker.Repository;
 
+import com.example.employeechecker.Model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface EmployeeRepository extends JpaRepository {
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 }
